59b0bcbc0317acce7c1fa933e78c3018cc3f6ba0
IamAndyF <63955000+IamAndyF@users.noreply.github.com>
2025-08-06 14:25:13 +0100

    Changed logger mechanism

--- logger.py
+++ logger.py
@@ -1,12 +1,11 @@
 import logging
 
-def setup_logger(name):
-    logger = logging.getLogger(name)
-    if not logger.handlers:
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
-
-    return logger
+logger = logging.getLogger("DocAssist")
+if not logger.handlers:
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
